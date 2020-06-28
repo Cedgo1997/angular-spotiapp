d318deb7c99336a15edeaa0a49c7e23e03317337
Cesar Gonzalez <cedgo1997@gmail.com>
2020-06-28 13:15:58 -0400

    spotify services requests centralized

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,27 +9,27 @@ import { map } from 'rxjs/operators';
 export class SpotifyService {
   constructor(private http: HttpClient) {}
 
-  getNewReleases() {
-    // HttpHeaders for set authorization for APIs:
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQBOeWI_rcOTmoDiwnFWvsfa8IDU7GGxALKqOithsMggw0aCrMP5LRs26ndcykt4ymp-bB0EDgMub0h4ANk',
+        'Bearer BQBWdMx8Ai6CFsqzmbbDfBPYbq9MO6My3H0j7hCZzu8JLzA0u0Q6aifcVH7x67jumppALHgvgDG7O4lE4Og',
     });
 
-    return this.http
-      .get('https://api.spotify.com/v1/browse/new-releases', { headers })
-      .pipe(map((data) => data['albums'].items));
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+    // HttpHeaders for set authorization for APIs:
+    return this.getQuery('browse/new-releases').pipe(
+      map((data) => data['albums'].items)
+    );
   }
 
   getArtist(term: string) {
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQBOeWI_rcOTmoDiwnFWvsfa8IDU7GGxALKqOithsMggw0aCrMP5LRs26ndcykt4ymp-bB0EDgMub0h4ANk',
-    });
-
-    return this.http
-      .get(`https://api.spotify.com/v1/search?q=${term}&type=artist&limit=15`, { headers })
-      .pipe(map((data) => data['artists'].items));
+    return this.getQuery(`search?q=${term}&type=artist&limit=15`).pipe(
+      map((data) => data['artists'].items)
+    );
   }
 }
